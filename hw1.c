40b222ad6b3b222226c9ea30938dc7d65592881d
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-03-13 02:24:36 +0800

    2st_want key change

--- hw1.c
+++ hw1.c
@@ -27,19 +27,19 @@ int try_pr(){
 }
 
 int scn_chk (char* tar_string ,int how_meny){
-size_t length_=strlen(tar_string);
-printf("%lu",length_);
+	size_t length_=strlen(tar_string);
+	printf("%lu",length_);
 
-if((int)length_==how_meny){
-printf("fit\n");
-return (1);
-}else{
-printf("not_fit\n");
-return (0);
-}
+	if((int)length_==how_meny){
+		printf("fit\n");
+		return (1);
+	}else{
+		printf("not_fit\n");
+		return (0);
+	}
 
 
-return (-1);
+	return (-1);
 }
 
 int main()
@@ -65,16 +65,59 @@ int main()
 	strcpy(six2,"456");
 	strcpy(six3,"789");
 
-scn_chk(ssp,8);
+	printf("統一發票兌獎系統\n");
+	printf("請輸入:\n");
+	printf("a. 特別獎\n");
+	scanf("%s",ssp); 
+	printf("b. 特獎\n");
+	scanf("%s",sp);
+	printf("c. 頭獎的 第一組號碼\n");
+	scanf("%s",hd1);
+	printf("d. 頭獎的 第二組號碼\n");
+	scanf("%s",hd2);
+	printf("e. 頭獎的 第三組號碼\n");
+	scanf("%s",hd3);
+	printf("f. 增開六獎的 第一組號碼\n");
+	scanf("%s",six1);
+	printf("g. 增開六獎的 第二組號碼\n");
+	scanf("%s",six2);
+	printf("h. 增開六獎的 第三組號碼\n");
+	scanf("%s",six3);
 
-printf("%s",sp);
+	printf("\n所有獎項\n");
+
+	printf("\na. 特別獎\n");
+	printf("%s",ssp); 
+	printf("\nb. 特獎\n");
+	printf("%s",sp);
+	printf("\nc. 頭獎的 第一組號碼\n");
+	printf("%s",hd1);
+	printf("\nd. 頭獎的 第二組號碼\n");
+	printf("%s",hd2);
+	printf("\ne. 頭獎的 第三組號碼\n");
+	printf("%s",hd3);
+	printf("\nf. 增開六獎的 第一組號碼\n");
+	printf("%s",six1);
+	printf("\ng. 增開六獎的 第二組號碼\n");
+	printf("%s",six2);
+	printf("\nh. 增開六獎的 第三組號碼\n");
+	printf("%s",six3);
+
+
+
+/*
+	scn_chk(ssp,8);
+
+	printf("%s",sp);
 	int ip=123;
-	
+
 	scanf("%d",&ip);
-size_t length=strlen(ssp);
-printf("字串長度%lu\n",length);
+	size_t length=strlen(ssp);
+	printf("字串長度%lu\n",length);
 	printf("%d",ip);
 	try_pr();
+*/
+
 	return 0;
 
 }

649153b6f8e4ca9ebb6c1d73c954e471896a3785
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-03-13 02:46:36 +0800

    input all ok

--- hw1.c
+++ hw1.c
@@ -5,7 +5,7 @@
 #define LEN 10
 
 /*
- * fuxk u r teacher
+ * 
  * sub fuction need put before main
  * 0ggVG=
  * need first initialization or will error
@@ -17,6 +17,8 @@
  * scan need & but print dont need
  * 
  * ...update when i recall
+ *
+ * 如果輸入錯誤，直接重來，沒有提示訊息!
  */
 
 int try_pr(){
@@ -28,13 +30,13 @@ int try_pr(){
 
 int scn_chk (char* tar_string ,int how_meny){
 	size_t length_=strlen(tar_string);
-	printf("%lu",length_);
+	//	printf("%lu",length_);
 
 	if((int)length_==how_meny){
-		printf("fit\n");
+		//		printf("fit\n");
 		return (1);
 	}else{
-		printf("not_fit\n");
+//		printf("not_fit\n");
 		return (0);
 	}
 
@@ -65,51 +67,51 @@ int main()
 	strcpy(six2," ");
 	strcpy(six3," ");
 	/*
-	strcpy(ssp,"12345678"); 
-	strcpy(sp,"87654321");
-	strcpy(hd1,"11111111");
-	strcpy(hd2,"22222222");
-	strcpy(hd3,"33333333");
-	strcpy(six1,"123");
-	strcpy(six2,"456");
-	strcpy(six3,"789");
-	*/
+	   strcpy(ssp,"12345678"); 
+	   strcpy(sp,"87654321");
+	   strcpy(hd1,"11111111");
+	   strcpy(hd2,"22222222");
+	   strcpy(hd3,"33333333");
+	   strcpy(six1,"123");
+	   strcpy(six2,"456");
+	   strcpy(six3,"789");
+	   */
 
 	printf("統一發票兌獎系統\n");
 	printf("請輸入:\n");
-	for(scn_chk( ssp,8)==0){
-	printf("a. 特別獎\n");
-	scanf("%s",ssp); 
+	while(scn_chk( ssp,8)==0){
+		printf("a. 特別獎\n");
+		scanf("%s",ssp); 
 	}
-		for(scn_chk(sp ,8)==0){
-	printf("b. 特獎\n");
-	scanf("%s",sp);
-		}
-			for(scn_chk(hd1 ,8)==0){
-	printf("c. 頭獎的 第一組號碼\n");
-	scanf("%s",hd1);
-			}
-				for(scn_chk(hd2 ,8)==0){
-	printf("d. 頭獎的 第二組號碼\n");
-	scanf("%s",hd2);
-				}
-					for(scn_chk( hd3,8)==0){
-	printf("e. 頭獎的 第三組號碼\n");
-	scanf("%s",hd3);
-					}
-						for(scn_chk( six1,8)==0){
-	printf("f. 增開六獎的 第一組號碼\n");
-	scanf("%s",six1);
-						}
-							for(scn_chk( six2,8)==0){
-	printf("g. 增開六獎的 第二組號碼\n");
-	scanf("%s",six2);
-							}
-								for(scn_chk(six3 ,8)==0){
-	printf("h. 增開六獎的 第三組號碼\n");
-	scanf("%s",six3);
-								}
-	
+	while(scn_chk(sp ,8)==0){
+		printf("b. 特獎\n");
+		scanf("%s",sp);
+	}
+	while(scn_chk(hd1 ,8)==0){
+		printf("c. 頭獎的 第一組號碼\n");
+		scanf("%s",hd1);
+	}
+	while(scn_chk(hd2 ,8)==0){
+		printf("d. 頭獎的 第二組號碼\n");
+		scanf("%s",hd2);
+	}
+	while(scn_chk( hd3,8)==0){
+		printf("e. 頭獎的 第三組號碼\n");
+		scanf("%s",hd3);
+	}
+	while(scn_chk( six1,3)==0){
+		printf("f. 增開六獎的 第一組號碼\n");
+		scanf("%s",six1);
+	}
+	while(scn_chk( six2,3)==0){
+		printf("g. 增開六獎的 第二組號碼\n");
+		scanf("%s",six2);
+	}
+	while(scn_chk(six3 ,3)==0){
+		printf("h. 增開六獎的 第三組號碼\n");
+		scanf("%s",six3);
+	}
+
 
 	printf("\n所有獎項\n");
 
@@ -132,18 +134,18 @@ int main()
 
 
 
-/*
-	scn_chk(ssp,8);
+	/*
+	   scn_chk(ssp,8);
 
-	printf("%s",sp);
-	int ip=123;
+	   printf("%s",sp);
+	   int ip=123;
 
-	scanf("%d",&ip);
-	size_t length=strlen(ssp);
-	printf("字串長度%lu\n",length);
-	printf("%d",ip);
-	try_pr();
-*/
+	   scanf("%d",&ip);
+	   size_t length=strlen(ssp);
+	   printf("字串長度%lu\n",length);
+	   printf("%d",ip);
+	   try_pr();
+	   */
 
 	return 0;
 

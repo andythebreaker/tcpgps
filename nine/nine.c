c9b5a65050b0645789e49f173a4d0d0d806ee863
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2019-04-25 12:52:09 +0800

    Add files via upload

--- nine/nine.c
+++ nine/nine.c
@@ -1,3 +1,10 @@
+/*
+ * =====NOTAM=====
+ * for string => size of
+ * need -1 (will auto +1 by system)
+ * ===============
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,12 +23,14 @@
 #define removeexample "r-0-0"
 #define mapping "111213212223313233182838342414373635272625171615"
 #define lengthofmapping 48
-#define inputlocateerror "The Location Does Not Exit"
+#define inputlocateerror "Input Location Is Not Available\n"
 #define greating "\n===Nine chess===\n"
 #define pleaseputyourchest "Enter where to put your chest:\n"
 #define player1 "player1"
 #define player2 "player2"
 #define thisisyourturn "This is your turn:"
+#define finishinputallplace "\n[Finished all input.]\n"
+#define linemapping ",7|12,12|16,4|11,11|19,1|10,10|22,9|13,13|18,2|5,5|8,17|20,20|23,3|15,15|24,6|14,14|21,1-2,2-3,4-5,5-6,7-8,8-9,10-11,11-12,13-14,14-15,16-17,17-18,19-20,20-21,22-23,23-24,"
 
 /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
@@ -45,6 +54,11 @@ int givethecofprint();
 void viewrawdata();
 int power(int tar, int i);
 void nextplayer();
+int numbersofmappliglineNEEDtime2();
+int asciitoint(int i);
+int if3dot(int changedpoint);
+int return4bit(int smallerthen99a, int smallerthen99b);
+int givealine(int index);
 
 /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
@@ -53,25 +67,37 @@ void nextplayer();
 
 int main()
 {
+    /*try*/
+
+    /*try*/
     printf("%s", greating);
     while (dataisfull() != 1)
     {
         whoisplayer();
         printf("%s", pleaseputyourchest);
         blankloop = 0;
-        while (put(inmod(1)) == -1)
+        int putedplace = inmod(1);
+        int putinmod1 = put(putedplace);
+
+        while (putinmod1 == -1)
         {
+            printf("%s", inputlocateerror);
+            printf("%s", pleaseputyourchest);
+            putedplace = inmod(1);
+            putinmod1 = put(putedplace);
             if (blankloop == 0)
             {
                 blankloop++;
                 blankloop--;
             }
-            printf("%s", inputlocateerror);
+
             blankloop++;
         }
         output_chart(OUTPUT_CHAR);
+        printf("If 3 dot happends :%d \n", if3dot(returnlocate(putedplace) + 1));
         nextplayer();
     }
+    printf("%s", finishinputallplace);
 
     return 0;
 }
@@ -85,12 +111,19 @@ int put(int locate)
 {
     //printf("locate:%d\n",locate);
     int place = returnlocate(locate);
-    //printf("place:%d\n",place);
     if (place == -1)
     {
         return -1;
     }
-    data[place] = currentplayer;
+    if (data[place] == 0)
+    {
+        data[place] = currentplayer;
+    }
+    else
+    {
+        return -1;
+    }
+
     return 0;
 }
 
@@ -267,7 +300,7 @@ int returnlocate(int yx)
 }
 int givethecofprint()
 {
-    if (thisisforprintindex == (lengthofmapping / 2) - 1)
+    if (thisisforprintindex == (lengthofmapping / 2))
     {
         thisisforprintindex = 0;
     }
@@ -350,4 +383,202 @@ void nextplayer()
     {
         currentplayer--;
     }
+}
+
+int numbersofmappliglineNEEDtime2()
+{
+    int numofa = 0;
+    int i;
+    for (i = 0; i < ((int)sizeof(linemapping) - 1); i++)
+    {
+        numofa = (((int)linemapping[i] == (int)'|') || ((int)linemapping[i] == (int)'-')) ? numofa + 1 : numofa;
+    }
+    return numofa;
+}
+
+int asciitoint(int i)
+{
+    return (i - ((int)'0'));
+}
+
+int if3dot(int changedpoint)
+{
+    int gotcha = 0;
+    int i;
+    for (i = 0; i < ((int)sizeof(linemapping) - 1); i++)
+    {
+        //printf("cp1!!!");
+        int whatiwant = (((int)linemapping[i] == (int)'|') || ((int)linemapping[i] == (int)'-')) ? 1 : 0;
+        if (whatiwant)
+        {
+            //printf("cp2!!!");
+            //printf("changepoints=:%d,###%d",changedpoint,i);
+
+            int givealine4bit = givealine(i);
+            //printf("\n<intgivealine4bitgivealine(i)>;%d\n",givealine4bit);
+
+            int parenum1 = givealine4bit / 100;
+            int parenum2 = givealine4bit % 100;
+            //printf("4bits:%d,%d<linemapping[i]:%c>",parenum1,parenum2,linemapping[i]);
+
+            if ((parenum1 == changedpoint) || (parenum2 == changedpoint))
+            {
+                //printf("cp3!!!");
+                int stat = (int)linemapping[i];
+                int k;
+                for (k = 0; k < ((int)sizeof(linemapping) - 1); k++)
+                {
+                    //printf("cp4!!!");
+                    int fit = ((int)linemapping[k] == stat) ? 1 : 0;
+                    if (fit)
+                    {
+                        //printf("cp5!!!");
+                        //printf("\npm:%d;%d\n;",parenum1,parenum2);
+                        int temp = givealine(k);
+                        int fit1 = temp / 100;
+                        int fit2 = temp % 100;
+                        //printf("givelinetraceback::input<%d>;;output_a=##%d__b=##%d\n",k,fit1,fit2);
+                        if (((fit1 == parenum1) || (fit2 == parenum1) || (fit1 == parenum2) || (fit2 == parenum2)) && (k != i))
+                        {
+                            //printf("cp6!!!");
+                            int nest4[4] = {0, 0, 0, 0};
+                            int nest3[3] = {0, 0, 0};
+                            nest4[0] = fit1;
+                            nest4[1] = fit2;
+                            nest4[2] = parenum1;
+                            nest4[3] = parenum2;
+                            //printf("nst4<%d;%d;%d;%d;>\n", fit1, fit2, parenum1, parenum2);
+                            int f;
+                            for (f = 0; f < 4; f++)
+                            {
+                                int comp = nest4[f];
+                                int ff = 0;
+                                int dontdo = 1;
+                                int have0 = 0;
+                                while (ff < 3)
+                                {
+                                    if (nest3[ff] == comp)
+                                    {
+                                        dontdo = 0;
+                                    }
+
+                                    ff++;
+                                }
+                                ff = 0;
+                                while (ff < 3)
+                                {
+                                    if (nest3[ff] == 0)
+                                    {
+                                        have0 = 1;
+                                        break;
+                                    }
+
+                                    ff++;
+                                }
+                                nest3[ff] = (have0 && dontdo) ? comp : nest3[ff];
+                            }
+                            /*heart~for 4 word have one same pick it out and put in to 3[] fuck  me up shit*/
+                            /*int forloop;
+                            for (forloop = 0; forloop < 4; forloop++)
+                            {
+                                int forloop1;
+                                int tar = nest4[forloop];
+                                for (forloop1 = 0; forloop1 < 4; forloop1++)
+                                {
+                                    nest4[forloop] = ((tar == nest4[forloop1])&&(tar!=forloop1)) ? -1 : tar;
+                                    //printf("nestU=<%d>\n",nest4[forloop]);
+                                }
+                                //printf("nestD=<%d>\n",nest4[forloop]);
+                            }
+                            int index3 = 0;
+                            for (forloop = 0; forloop < 4; forloop++)
+                            {
+                                if (nest4[forloop] != -1)
+                                {
+                                    nest3[index3] = nest4[forloop];
+                                    index3++;
+                                    printf("nest=<%d>",nest3[index3]);
+                                }
+                                //printf("nestU=<%d>\n",nest4[forloop]);
+                            }*/
+                            int ap;
+                            for (ap = 0; ap < 3; ap++)
+                            {
+                                //printf("==>%d", nest3[ap]);
+                            }
+                            //printf("\n");
+                            int forloop;
+                            int seeIfAllNoEqualsToCurrentPlayers = 0;
+                            for (forloop = 0; forloop < 3; forloop++)
+                            {
+                                //printf("data:%d;cur:%d;;num:%d\n", data[nest3[forloop] - 1], currentplayer, nest3[forloop]);
+                                seeIfAllNoEqualsToCurrentPlayers = (data[nest3[forloop] - 1] != currentplayer) ? seeIfAllNoEqualsToCurrentPlayers : seeIfAllNoEqualsToCurrentPlayers + 1;
+                            }
+                            gotcha = (seeIfAllNoEqualsToCurrentPlayers == 3) ? gotcha + 1 : gotcha;
+                            //printf("checkpoint\n");
+                        }
+                    }
+                }
+            }
+        }
+    }
+    int ifnot0return1 = (gotcha == 0) ? 0 : 1;
+    return ifnot0return1;
+}
+
+int return4bit(int smallerthen99a, int smallerthen99b)
+{
+
+    int a = smallerthen99a;
+    int b = smallerthen99b;
+
+    //printf("\na:%d,b:%d\n",a,b);
+    return (a * 100 + b);
+}
+
+int givealine(int index)
+{
+    //printf("giveline:input@@@%d\n",index);
+    int indexreminder = index;
+    int indexdontmove = index;
+    int buf = 0;
+    int matha = 0;
+    int mathb = 0;
+    int counter = 0;
+    index--;
+    buf = (int)linemapping[index];
+    while (buf != ((int)','))
+    {
+        // printf("buf%dbuf",asciitoint(buf));
+        matha += asciitoint(buf) * power(10, counter);
+        counter++;
+        index--;
+        buf = (int)linemapping[index];
+    }
+    //counter = 1;
+    indexreminder++;
+    buf = (int)linemapping[indexreminder];
+    while (buf != ((int)','))
+    {
+        // printf("buf%dbuf",asciitoint( buf ));
+        //mathb += asciitoint(buf) * power(10, counter);
+        //counter--;
+        indexreminder++;
+        buf = (int)linemapping[indexreminder];
+    }
+    counter = 0;
+    indexreminder--;
+    buf = (int)linemapping[indexreminder];
+    //printf("pass");
+    while (buf != ((int)linemapping[indexdontmove]))
+    {
+        // printf("buf%dbuf",asciitoint(buf));
+        mathb += asciitoint(buf) * power(10, counter);
+        counter++;
+        indexreminder--;
+        buf = (int)linemapping[indexreminder];
+    }
+
+    //printf("giveline:a:%d,b:%d\n",matha,mathb);
+    return return4bit(matha, mathb);
 }
